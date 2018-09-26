94d79db24523d015d69c3d3b821ad2a300407880
bradclegg <bradclegg18@gmail.com>
2018-09-26 17:38:54 -0600

    hopefully its right

--- web/ta2.js
+++ web/ta2.js
@@ -3,8 +3,8 @@ function clickMe() {
 }
 
 function changeColor() {
-    var textColor = document.getElementsByClassName("textColor");
-    var div1 = document.getElementsByClassName("first");
+    var textColor = document.getElementsByClassName(textColor);
+    var div1 = document.getElementsByClassName(first);
 
     div1.style.color = textColor;
 }
