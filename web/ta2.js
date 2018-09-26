26c003d62f247014a94065d1e8badce52d9e4a14
bradclegg <bradclegg18@gmail.com>
2018-09-26 17:31:53 -0600

    almost done

---
+++ web/ta2.js
@@ -0,0 +1,10 @@
+function clickMe() {
+	alert("Clicked!");
+}
+
+function changeColor() {
+    var textColor = document.getElementsByClassName("textColor");
+    var div1 = document.getElementsByClassName("first");
+
+    div1.style.color = textColor;
+}
