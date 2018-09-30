cedbb900a1244acf5e485240e5eaa17ecb5ca541
bradclegg <bradclegg18@gmail.com>
2018-09-29 20:12:27 -0600

    adding homepage

---
+++ web/homepage/assignments.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Assignments</title>
+    <meta name="author" content="Brad">
+    <link rel="stylesheet" href="web/styles.css">
+
+</head>
+<body>
+    <?php include(includes/nav.php)?>
+    <h1>Coming Soon</h1>
+</body>
+</html>
---
+++ web/homepage/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="homepage.php">Home</a></li>
+        <li><a href="assignments.php">Assignments</a></li>
+        <li><img src="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj6wv6Tz-HdAhWIHzQIHTjVCEoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.iconspedia.com%2Ficon%2Fsuper-paper-mario-25-2.html&psig=AOvVaw3Vurud8C7V-00eQouvuiyO&ust=1538358766776488"></li>
+    </ul>
+</nav>
