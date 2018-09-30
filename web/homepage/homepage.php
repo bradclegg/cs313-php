77478ee9db9dcdd24136364e9a509041ad575844
bradclegg <bradclegg18@gmail.com>
2018-09-29 20:18:31 -0600

    edit

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -8,7 +8,7 @@
 
 </head>
 <body>
-    <?php include('includes/nav.php') ?>
+    <?php include('nav.php') ?>
     <h1>Hey Im Brad and I think Mario is cool</h1>
     <div>
         <img src="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-5c2j0OHdAhXZJTQIHT2sDMQQjRx6BAgBEAU&url=http%3A%2F%2Fmariokartwii.wikia.com%2Fwiki%2FCoin_Box&psig=AOvVaw0dntuFFBDrjgVsPF1cpBH0&ust=1538359063614038">
