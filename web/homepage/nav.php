025afc6c54bf48f0c3bdfc9238e665394680366a
bradclegg <bradclegg18@gmail.com>
2018-09-29 20:47:29 -0600

    edit

--- web/homepage/nav.php
+++ web/homepage/nav.php
@@ -2,6 +2,6 @@
     <ul>
         <li><a href="homepage.php">Home</a></li>
         <li><a href="assignments.php">Assignments</a></li>
-        <li><img src="mario.png"></li>
+        <li><img class="small" src="mario.png"></li>
     </ul>
 </nav>
