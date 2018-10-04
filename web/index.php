1a5ec767eeb68e63aaa4ea2a71950440f2a7f99b
bradclegg <bradclegg18@gmail.com>
2018-10-03 22:38:21 -0600

    gg

--- web/index.php
+++ web/index.php
@@ -1,5 +1,22 @@
 <?php
-
-  phpinfo();
-
+//ta02.php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$major = $_POST['major'];
+$comment = $_POST['comment'];
+$continent = $_POST['continent'];
+echo "<h1>Your Input:</h1>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $major;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo "Continents Visited: <br>";
+foreach ($continent as $continentName){
+	echo $continentName;
+	echo "<br>";
+}
 ?>
