f417d509a5c4067095f984f03f7b051d5bfb6f27
bradclegg <bradclegg18@gmail.com>
2018-10-08 19:31:04 -0600

    gg

--- web/homepage/week3/prove3.php
+++ web/homepage/week3/prove3.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <title>week 3 prove</title>
     <meta name="author" content="Brad">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../styles.css">
 
 </head>
 <body>
-    <?php include('../../nav.php')?>
+    <?php include('../nav.php')?>
 </body>
 </html>
