15e1e8cee3c5d263cd5e3abcf5afd94f2e203355
bradclegg <bradclegg18@gmail.com>
2018-10-08 20:47:27 -0600

    gg

--- web/homepage/week3/prove3.php
+++ web/homepage/week3/prove3.php
@@ -17,6 +17,22 @@
         <img src="images/life.jpg">
         <p>One life mushroom</p>
     </div>
+    <div class="firelower">
+        <img src="images/fire.jpg">
+        <p>One fire flower/p>
+    </div>
+    <div class="greenShell">
+        <img src="images/greenShell.jpg">
+        <p>One green shell</p>
+    </div>
+    <div class="blueShell">
+        <img src="images/blueShell.jpg">
+        <p>One blue shell</p>
+    </div>
+    <div class="star">
+        <img src="images/star.jpg">
+        <p>One super star</p>
+    </div>
 
 </body>
 </html>
