b8d3306f59df26299a43b49f80303e174c41f0b5
bradclegg <bradclegg18@gmail.com>
2018-10-08 21:07:13 -0600

    gg

--- web/homepage/week3/prove3.php
+++ web/homepage/week3/prove3.php
@@ -1,42 +1,42 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>week 3 prove</title>
-    <meta name="author" content="Brad">
-    <link rel="stylesheet" href="../styles.css">
+    <head>
+        <meta charset="utf-8">
+        <title>week 3 prove</title>
+        <meta name="author" content="Brad">
+        <link rel="stylesheet" href="../styles.css">
 
-</head>
-<body>
-    <?php include('../nav.php')?>
-    <div class="line1">
-        <div class="mushroom">
-            <img src="images/mushroom.jpg">
-            <p>One mushroom</p>
+    </head>
+    <body>
+        <?php include('../nav.php')?>
+        <div class="line1">
+            <div class="mushroom">
+                <img src="images/mushroom.jpg">
+                <p>One mushroom</p>
+            </div>
+            <div class="lifeMushroom">
+                <img src="images/life.jpg">
+                <p>One life mushroom</p>
+            </div>
+            <div class="firelower">
+                <img src="images/fire.jpg">
+                <p>One fire flower</p>
+            </div>
         </div>
-        <div class="lifeMushroom">
-            <img src="images/life.jpg">
-            <p>One life mushroom</p>
+        <div class="line2">
+            <div class="greenShell">
+                <img src="images/greenShell.jpg">
+                <p>One green shell</p>
+            </div>
+            <div class="blueShell">
+                <img src="images/blueShell.jpg">
+                <p>One blue shell</p>
+            </div>
+            <div class="star">
+                <img src="images/star.jpg">
+                <p>One super star</p>
+            </div>
         </div>
-        <div class="firelower">
-            <img src="images/fire.jpg">
-            <p>One fire flower</p>
-        </div>
-    </div>
-    <div class="line2">
-        <div class="greenShell">
-            <img src="images/greenShell.jpg">
-            <p>One green shell</p>
-        </div>
-        <div class="blueShell">
-            <img src="images/blueShell.jpg">
-            <p>One blue shell</p>
-        </div>
-        <div class="star">
-            <img src="images/star.jpg">
-            <p>One super star</p>
-        </div>
-    </div>
 
-</body>
+    </body>
 </html>
