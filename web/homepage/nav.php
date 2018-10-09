047e684d9ce807b3e7cc9322bc5c6b13619a8fee
bradclegg <bradclegg18@gmail.com>
2018-10-08 21:53:08 -0600

    gg

--- web/homepage/nav.php
+++ web/homepage/nav.php
@@ -3,6 +3,6 @@
         <li><a href="/homepage/homepage.php">Home</a></li>
         <li><a href="/homepage/assignments.php">Assignments</a></li>
         <li><img class="small" src="/homepage/mario.png"></li>
-        <li><a href="week3/cart.php">Cart</a></li>
+        <li><a href="cart.php">Cart</a></li>
     </ul>
 </nav>
