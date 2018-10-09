4c7cb07159a3ed6738fb5a04e14a2824313c8d04
bradclegg <bradclegg18@gmail.com>
2018-10-08 20:11:43 -0600

    gg

--- web/homepage/nav.php
+++ web/homepage/nav.php
@@ -2,6 +2,6 @@
     <ul>
         <li><a href="/homepage/homepage.php">Home</a></li>
         <li><a href="/homepage/assignments.php">Assignments</a></li>
-        <li><img class="small" src="mario.png"></li>
+        <li><img class="small" src="/homepage/mario.png"></li>
     </ul>
 </nav>
