07e55113872502a651d99e9b5cfe00c47094fd90
bradclegg <bradclegg18@gmail.com>
2018-10-08 22:43:02 -0600

    gg

--- web/homepage/week3/cart.php
+++ web/homepage/week3/cart.php
@@ -1,11 +1,11 @@
 <?php
 	session_start();
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Assignments</title>
+    <title>Cart</title>
     <meta name="author" content="Brad">
     <link rel="stylesheet" href="../styles.css">
 
@@ -13,14 +13,14 @@
 <body>
     <?php include('../nav.php')?>
     <?php
-    			if ($_SESSION["cart"] == 0) {
-                    echo "<p> cart is empty </p>";
-                }
-              
-                if ($_SESSION['item1'] > 0) {
-					echo "<div class='mushroom'>";
-                    echo "<img src='images/mushroom.jpg'>";
-                    echo "<p>One mushroom</p>";
-     ?>
+    if ($_SESSION["cart"] == 0) {
+        echo "<p> cart is empty </p>";
+    }
+  
+    if ($_SESSION['item1'] > 0) {
+        echo "<div class='mushroom'>";
+        echo "<img src='images/mushroom.jpg'>";
+        echo "<p>One mushroom</p>";
+                    ?>
 </body>
 </html>
